b4d6fb0dee6fe43e00f9d0c19ad3089aba1a4284
Apple <apple@Apples-MacBook-Pro.local>
2019-04-28 10:52:23 +0530

    Commented download statements in Antonyms and Synonyms

--- application.py
+++ application.py
@@ -1,33 +1,13 @@
 from flask import Flask, render_template, request, flash
-import area
+# import area
 import Synonyms
 import Antonyms
 
-# from datetime import datetime
-# import re
-#app = Flask(__name__)
-# app.secret_key = 'b_5#y2L"F4Q8z\n\xec]/'
-# @app.route('/')
-# def homepage():
-#         return render_template("HomePage.html")
-
-# if __name__ == '__main__':
-#      app.run()
-
 app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('HomePage.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-# @app.route('/', methods=["POST"])
-# def some_function():
-#     text = request.form.get('textbox')
-#     print(text)
-
+    
 app.secret_key = 'b_5#y2L"F4Q8z\n\xec]/'
 @app.route('/', methods=["GET","POST"])
 def login_page():
